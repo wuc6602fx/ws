4a4b2f9383f0c56362d0484ad710032ce8db60bb
wuc6602fx <cat5530xm@gmail.com>
2017-10-22 16:40:25 +0800

    finish q2.py

--- q2.py
+++ q2.py
@@ -1,1 +1,25 @@
 #ques2
+from collections import OrderedDict
+#tuple
+myTuple = list()
+with open('example.csv') as f:
+    for line in f.read().splitlines():  #avoid \r\n
+        myTuple.append(line.split(', '))
+myTuple = sorted(myTuple)   #by alphblet
+myTuple.pop(len(myTuple)-1) #remove header
+print 'Tuple = ', myTuple
+#dict
+scores = [x[2] for x in myTuple]
+counts = [0, 0, 0, 0]
+for score in scores:
+    if int(score)/10 == 9:
+        counts[3] = counts[3] + 1
+    elif int(score)/10 == 8:
+        counts[2] = counts[2] + 1
+    elif int(score)/10 == 7:
+        counts[1] = counts[1] + 1
+    elif int(score)/10 == 6:
+        counts[0] = counts[0] + 1
+myRange = ['60~69', '70~79', '80~89', '90~99']
+myDict = OrderedDict(zip(myRange, counts))  #70~79 90~99 80~89 60~69, if no use ordereddict
+print myDict
