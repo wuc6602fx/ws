eac20a05d1df5a694f99dbebf631058f3473e3c1
wuc6602fx <cat5530xm@gmail.com>
2017-11-06 15:04:39 +0800

    [ADD]RPC of RabbitMQ

--- receive.py
+++ receive.py
@@ -1,6 +1,5 @@
 import pika
 
-
 # connect to RabbitMQ server, same as send.py
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -8,15 +7,12 @@ channel = connection.channel()
 # create queue
 channel.queue_declare(queue='hello')
 
-
 # make a callback function
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
 
-
 # notify server use it as callback
-channel.basic_consume(callback, queue='hello')
-
+channel.basic_consume(callback, queue='hello', no_ack=True)
 
 # start to receive
 channel.start_consuming()
