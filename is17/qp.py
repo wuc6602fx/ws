eb39476a8b05e94933b29ca180b3289cf719528d
wuc6602fx <cat5530xm@gmail.com>
2017-11-15 00:49:47 +0800

    [FIX]IS17mid queen problem finish

--- is17/qp.py
+++ is17/qp.py
@@ -1,5 +1,8 @@
 K = 8   #Board size
 P = 8   #Queen force
+pre = [(4,5),(7,7)] #prelocate
+#pre = []
+
 
 def under_attack(col,queens):
     left = right = col
@@ -11,6 +14,7 @@ def under_attack(col,queens):
             return True
     return False
 
+
 #Recurrsive function to solve K-queens problem
 def solve(n):
     if n == 0:
@@ -21,22 +25,47 @@ def solve(n):
                 for solution in smaller_solutions
                     if not under_attack(i+1, solution)]
 
+
 #Method to display chess board
 def display_board(queens):
     q_icon =  "♕"
-    print('='*K*(K+1))
+    print('='*64)
     for i in range(1,K+1):
         print(K-i+1,'\t|'*(queens[i-1][1]),' ',q_icon,'  |','\t|'*(K-queens[i-1][1]))
         if i == K:
-            print('='*K*(K+1))
+            print('='*64)
         else:
-            print("-"*K*(K+1))
+            print("-"*64)
 
-count=0
-for answer in solve(K):
-    if(count<=50):
-        display_board(answer)
-    count = count + 1
-print('K = ',K,' P = ',P,' has ',count,' solutions.')
-if(count>50):
+
+#for calculate, we rotate the board 90 degrees with clockwise
+def rotate_board(queens):
+    rlt = []
+    for q in queens:
+        rlt.append((K - q[1] + 1, q[0]))
+    return rlt
+
+
+r_pre = rotate_board(pre)
+answers = solve(K)
+#if member in pre not in the answer, the answer should be remove
+if len(pre)>0:
+    new_answers = []
+    for a in answers:
+        for p in r_pre:
+            if p in a:
+                if p == r_pre[-1]:
+                    new_answers.append(a)
+            else:
+                break
+    answers = new_answers
+
+counts = len(answers)
+print('K = ',K,' P = ',P,' has ',counts,' solutions.')
+for answer in answers:
+    display_board(answer)
+    print(answer,'\n')
+if len(pre)>0:
+    print('Prelocate = ',pre)
+if len(answers)>50:
     print('The above only show first 50 solutions.')
